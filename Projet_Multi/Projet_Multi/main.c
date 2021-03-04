ecc8f7cac90c1f8881bd10807d76742ecd328e1e
Jacob <jacobcoss7@gmail.com>
2021-03-04 12:59:10 -0500

    Update main description

--- Projet_Multi/Projet_Multi/main.c
+++ Projet_Multi/Projet_Multi/main.c
@@ -1,8 +1,14 @@
 /*
+ *OBJECTIF: Concevoir et réaliser un véhicule télécommandé pouvant lancer des petits
+ *	disques sur des cibles ainsi que sa manette de contrôle dans un délai de 15 semaines. 
+ *	Par la suite, vous ferez la démonstration des performances de votre prototype lors d’
+ *	une compétition organisée durant la période des examens finaux. La suite du cahier des 
+ *	charges décrit les conditions et contraintes auxquelles votre projet devra répondre.
+ *
  * Projet_Multi.c
  *
  * Created: 3/4/2021 12:03:21 PM
- * Author : Richard
+ * Author : Jacob
  */ 
 
 #include <avr/io.h>
