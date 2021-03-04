a8a7facff69ee2c11b440a90c9ffab62315c5c7a
Jacob <jacobcoss7@gmail.com>
2021-03-04 12:30:20 -0500

    Add Project MAIN AND LIBRARY
    Driver, fifo, lcd, main, uart, utils

---
+++ Projet_Multi/Projet_Multi/main.c
@@ -0,0 +1,18 @@
+/*
+ * Projet_Multi.c
+ *
+ * Created: 3/4/2021 12:03:21 PM
+ * Author : Richard
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
